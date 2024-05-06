424b755ee71d2c76081e8ab80c82faccd2e39593
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-06 23:40:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 #TYPE CASTING = convert data type of a value to another data type
 
-#x=5     #int
-#y=3.0   #float
-#z="7"   #str
+x=5     #int
+y=3.0   #float
+z="7"   #str
 
-#y=int(y)
-#z=int(z)
+y=int(y)
+z=int(z)
 
-#x=float(x)
-#y=float(y)
-#z=float(z)
+x=float(x)
+y=float(y)
+z=float(z)
 
-#print(x)
-#print(y)
-#print(int(y))
-#print(z*4)
+print(x)
+print(y)
+print(int(y))
+print(z*4)
 
 #when whould you need to convert a integer or float to a string?
-#print("X is "+ str(x))
-#print("Y is "+str(y))
+print("X is "+ str(x))
+print("Y is "+str(y))
 #you need to type cast when you need to reprent a string along with integer or float
 
 
